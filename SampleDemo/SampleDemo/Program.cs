5ff5c34dc17e971a26b93243dbe4e7030567fe3a
Ankur Pathak <ankur.ats@hotmail.com>
2021-07-05 11:16:29 +0530

    Intitial Project

---
+++ SampleDemo/SampleDemo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SampleDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
